5a9dbe74648eef6dfa911ee0161e713e8b46b0bd
Manuel-Campoy <mcampoyteran5@gmail.com>
2025-11-09 20:08:31 -0700

    first commit

---
+++ frontend/src/App.jsx
@@ -0,0 +1,48 @@
+import { useState } from 'react';
+import axios from 'axios';
+import { BrowserRouter as Router, Routes, Route, Navigate } from 'react-router-dom';
+import Navbar from './components/Navbar';
+import Home from './pages/Home';
+import Login from './pages/Login';
+import Register from './pages/Register';
+
+axios.defaults.withCredentials = true;
+
+function App(){
+
+  const [user, setUser] = useState(null);
+  const [error, setError] = useState("");
+  const [loading, setLoading] = useState(true);
+
+  useEffect(() => {
+    const fetchUser = async () => {
+      try {
+        const res = await axios.get('/api/auth/me');
+        setUser(res.data);
+      } catch (err) {
+        setUser(null);
+      } finally {
+        setLoading(false);
+      }
+    }
+    fetchUser();
+  }, []);
+
+  if (loading) {
+    return <div className='bg-gray-800 min-h-screen'>Loading...</div>;
+  }
+
+  return (
+    <Router>
+      <Navbar user={user} setUser={setUser}/>
+      <Routes>
+        <Route path='/' element={<Home user={user} error={error}/>} />
+        <Route path='/login' element={user ? <Navigate to="/" /> : <Login setUser={setUser}/>} />
+        <Route path='/register' element={user ? <Navigate to="/" /> : <Register setUser={setUser} />} />
+        <Route path="*" element={<NotFound />} />
+      </Routes>
+    </Router>
+  )
+}
+
+export default App;
---
+++ frontend/src/components/Navbar.jsx
@@ -0,0 +1,33 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+import axios from 'axios';
+
+const Navbar = ({user, setUser}) => {
+    const navigate = useNavigate();
+
+    const handleLogout = async () => {
+        await axios.post('/api/auth/logout');
+        setUser(null);
+        navigate('/');
+    }
+
+    return (
+        <nav className='bg-gray-800 p-4 text-white'>
+            <div className='max-w-6x1 mx-auto p-4 flex justify-between items-center'>
+                <Link to="/" className="font-bold">PERN Auth</Link>
+            <div>
+                {user ? (
+                    <button oncClick={handleLogout} className='bg-red-500 px-3 py-1 rounded'>Logout</button>
+                ) : (
+                    <>
+                    <Link to='/login' className='mx-2'>Login</Link>
+                    <Link to='/register' className='mx-2'>Register</Link>
+                    </>
+                )}
+            </div>
+            </div>
+        </nav>
+    )
+}
+
+export default Navbar;
